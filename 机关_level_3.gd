e5cd2005f9be072164c85032aefa921d4b352280
Nathan-wang235 <25644@burnside.school.nz>
2026-03-19 12:25:59 +1300

    an interactiveable thing added

--- 机关_level_3.gd
+++ 机关_level_3.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+@export var wall_node : Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +10,11 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.name == "Rock_area_level3" :
+		print("test successful")
+		wall_node.queue_free()
