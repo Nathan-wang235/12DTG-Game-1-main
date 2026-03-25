cab44555dd487ff2eedba382cf3838c80dae62ac
Mingxuan-ggg <24750@burnside.school.nz>
2026-03-26 09:07:03 +1300

    music

--- player_level_2.gd
+++ player_level_2.gd
@@ -13,7 +13,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+		$Jumpsound.play()
 	# Get the input direction and handle the movement/deceleration.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	print(direction)
